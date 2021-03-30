e7941285ff7561205fdf9fcb4ee8832d1c5c5acf
Vinayak <sareenv026@outlook.com>
2021-03-30 21:34:29 +0530

    changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 
 const express = require('express')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 const app = new express()
 
 const port = process.env.PORT || 8080
@@ -31,5 +32,32 @@ app.get('/fail', (req, res) => {
     process.exit(1)
 })
 
+/*
+ Db connection testing and stuff.
+* */
+
+async function dbConnection() {
+    try {
+        const {username, password, dbUrl} = process.env
+        const url = `mongodb://${username}:${password}@${dbUrl}`
+        console.log(url)
+        const result = await mongoose.connect(url)
+        console.log("The result result was " + result)
+        return {username, password, dbUrl}
+    } catch (error) {
+        console.log("It was fail")
+        throw new Error(error)
+    }
+}
+
+app.get('/db-details', async (req, res) => {
+    try {
+        const result = await dbConnection();
+        res.send({result})
+    }catch (error) {
+        const {username, password, dbUrl} = process.env
+        res.send({error, username, password, dbUrl})
+    }
+})
 
 app.listen(port, () => "Server is listening on default port")
