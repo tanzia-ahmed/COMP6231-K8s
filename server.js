a4bf75750682b7c8b34af43e09984f3da760418e
Vinayak <sareenv026@outlook.com>
2021-04-08 16:28:16 +0530

    changes

--- server.js
+++ server.js
@@ -7,19 +7,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const MongoClient = require('mongodb').MongoClient;
 const app = new express()
 
 const port = process.env.PORT || 8080
 dotenv.config()
 
-app.get('/db-2', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) { return res.send(err) }
-        res.send("Database created!")
-        db.close();
-    });
-})
 
 
 app.get('/', (req, res) => {
@@ -43,7 +35,6 @@ app.get('/fail', (req, res) => {
 })
 
 
-const url = "mongodb://mongo-0.mongo.mongo.svc.cluster.local:27017,mongo-1.mongo.mongo.svc.cluster.local:27017,mongo-2.mongo.mongo.svc.cluster.local:27017/dbname_?replicaSet=rs0`
 
 
 /*
@@ -52,28 +43,13 @@ const url = "mongodb://mongo-0.mongo.mongo.svc.cluster.local:27017,mongo-1.mongo
 
 async function dbConnection() {
     try {
-        // test this connection.
-        await mongoose.createConnection(url, {
+        const url = "mongodb://mongo-0.mongo-service.default.svc.cluster.local,mongo-1.mongo-service.default.svc.cluster.local,mongo-2.mongo-service.default.svc.cluster.local:27017/db_name?replicaSet=rs0"
+        await mongoose.createConnection(url,  {
             useNewUrlParser: true,
-            db: {native_parser: true},
-            replset: {
-                auto_reconnect: false,
-                poolSize: 10,
-                readPreference: "secondary",
-                socketOptions: {
-                    keepAlive: 1000,
-                    connectWithNoPrimary: true,
-                    connectTimeoutMS: 30000
-                },
-                server: {
-                    poolSize: 5,
-                    socketOptions: {
-                      keepAlive: 1000,
-                      connectTimeoutMS: 30000
-                    }
-                }
-            }
-        })
+            dbName: 'myDb' // Specify the database here
+          }          
+        )
+        console.log("Connected to database")
         return true
     } catch (error) {
         console.log(error)
@@ -91,5 +67,12 @@ app.get('/db-details', async (req, res) => {
         res.send({"Message": "Connection error ", error})
     }
 })
+
+process.on('SIGINT', function() {  
+    mongoose.connection.close(function () { 
+      console.log("Moongoose disconnecting the database"); 
+      process.exit(0); 
+    }); 
+  }); 
 
 app.listen(port, () => "Server is listening on default port")
